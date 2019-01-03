af96f3b9e1c33e13116399bcac852b53f39dee0a
IanDuncanT <duncan.ian.t@gmail.com>
2019-01-03 14:57:50 -0600

    Unmount dmg file and add dropbox desktop install

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -3,5 +3,6 @@ wget -O discord.dmg https://discordapp.com/api/download?platform=osx
 hdiutil mount discord.dmg
 cd /Volumes/Discord
 sudo cp -R Discord.app /Applications/Discord.app/
+hdiutil unmount /Volumes/Discord
 cd ~/YAPI
 rm scripts/discord.dmg
---
+++ scripts/dropbox.sh
@@ -0,0 +1,8 @@
+# Dropbox desktop application - https://discordapp.com/
+wget -O dropbox.dmg https://www.dropbox.com/download?os=mac
+hdiutil mount dropbox.dmg
+cd "/Volumes/Dropbox Installer"
+open Dropbox
+hdiutil unmount "/Volumes/Dropbox Installer"
+cd ~/YAPI
+rm scripts/dropbox.dmg
