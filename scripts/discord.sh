f88eff382eb27274d196170ab3ec0451aa9fda4d
IanDuncanT <duncan.ian.t@gmail.com>
2019-01-03 14:24:24 -0600

    print pwd

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -1,7 +1,6 @@
 # Discord chat vocale e testuale - https://discordapp.com/
 wget -O discord.dmg https://discordapp.com/api/download?platform=osx
 hdiutil mount discord.dmg
-echo "pwd"
 cd /Volumes/Discord
 sudo cp -R Discord.app /Applications/Discord.app/
 cd ~/YAPI
