2f4ed27b1fdd91cd337bfc4b890de4f05710b71f
IanDuncanT <duncan.ian.t@gmail.com>
2019-01-03 14:08:07 -0600

    report pwd

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -1,6 +1,6 @@
 # Discord chat vocale e testuale - https://discordapp.com/
 wget -O discord.dmg https://discordapp.com/api/download?platform=osx
 hdiutil mount discord.dmg
-pwd
+echo pwd
 cd /Volumes/Discord
 cp -R Discord.app /Applications/Discord.app/
